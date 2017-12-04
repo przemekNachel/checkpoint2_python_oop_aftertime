f120ef433bb7516c458e0a422ac09cfd75eed61c
przemekNachel <przemeknachel@gmail.com>
2017-12-04 22:40:43 +0100

    Add inheritance

--- work_address.py
+++ work_address.py
@@ -1,4 +1,7 @@
-class WorkAddress:
+from address import Address
+
+
+class WorkAddress(Address):
 
     def __init__(self, person, city, street, house_no, company):
         self.person = person
@@ -9,25 +12,3 @@ class WorkAddress:
 
     def get_full_address(self):
         return "{}, {}, {} {}, {}".format(self.person, self.city, self.street, self.house_no, self.company)
-
-    def __gt__(self, other):
-        return self.get_full_address() > other.get_full_address()
-
-    def __ge__(self, other):
-        return self.get_full_address() >= other.get_full_address()
-
-    def __lt__(self, other):
-        return self.get_full_address() < other.get_full_address()
-
-    def __le__(self, other):
-        return self.get_full_address() <= other.get_full_address()
-
-    def __eq__(self, other):
-        return self.get_full_address() == other.get_full_address()
-
-    def __ne__(self, other):
-        return self.get_full_address() != other.get_full_address()
-
-    def __str__(self):
-        address = self.get_full_address()
-        return address
